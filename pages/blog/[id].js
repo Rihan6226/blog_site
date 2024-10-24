947510bdff14e601fd874b60316c9f0a43f7cf07
Rihan6226 <mohammedrihan6226@gmail.com>
2024-10-24 13:53:29 +0530

    mail change

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -110,7 +110,7 @@ const Post = ({ post, user }) => {
         )}
       </section>
 
-      { (user.name === post.author || user.email === 'sansmarotia@gmail.com') && (
+      { (user.name === post.author || user.email === 'sanskritiagarwal0810@gmail.com') && (
         <div className="mt-8 flex space-x-4">
 
           <button
